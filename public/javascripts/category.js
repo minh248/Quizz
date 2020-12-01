925ad0bfc06d19147c9a11cf377230e3eec0bce5
DESKTOP-A9KGAOK\AD <ducmin248@gmail.com>
2020-12-01 08:50:00 +0700

    update

--- public/javascripts/category.js
+++ public/javascripts/category.js
@@ -1,12 +1,3 @@
-function check() {
-    var c = 0;
-    var q1 = document.quizz.animals1.value;
-    var q2 = document.quizz.animals2.value;
-    if (q1 == "The wild kittens of domestic cats who have never lived with humans.") { c++ }
-    if (q2 == "All of these") { c++ }
-    document.write(c);
-}
-
 var mockApiUrl = 'https://5fbfb63cfd14be0016748f86.mockapi.io/api/v1/quizz'
 var catergoryUrl = 'http://127.0.0.1:5000/category'
 
